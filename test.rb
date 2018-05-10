d512da5c0d6c38fd668388f2b3852d714b6a0494
Amar <digitallyamar@gmail.com>
2018-05-11 00:23:40 +0530

    Adding content from master branch, while content in testing branch is not yet merged, so those changes are still isolated from master branch's test.rb file

--- test.rb
+++ test.rb
@@ -1,1 +1,3 @@
 //Dummy initial comment
+
+//Changing content again in master branch. But notice changes done in testing branch doesnt appear here as you are in master branch and testing branch content is not merged back here
